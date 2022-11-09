d380710945bd3c462b1e9c14c3c3395f6e33096a
Spammer92 <113533295+Spammer92@users.noreply.github.com>
2022-11-09 07:37:39 +0100

    Add files via upload

--- js/ini.js
+++ js/ini.js
@@ -1,11 +1,3 @@
-if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|BB|PlayBook|IEMobile|Windows Phone|Kindle|Silk|Opera Mini/i.test(navigator.userAgent)) {
-    document.getElementById("header-desktop").style.display = "none";
-    document.getElementById("header-mobile").style.display = "grid";
-} else {
-    document.getElementById("header-desktop").style.display = "flex";
-    document.getElementById("header-mobile").style.display = "none";
-}
-
 function openGithub() {
     window.open("https:github.com/Spammer92");
 }
